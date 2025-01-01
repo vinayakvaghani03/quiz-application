c2fd084ee27fe48c83a267fc662b8b1a63e0822f
vinayakvaghani03 <vinayakwork8@gmail.com>
2025-01-01 17:42:37 +0530

    backend-done

--- backend/router/router.js
+++ backend/router/router.js
@@ -1,10 +1,12 @@
 const express=require("express")
 const router=express.Router()
-const {questionapi,questionpostapi,dropquestionapi}=require("../controllers/controller")
+const {questionapi,questionpostapi,dropquestionapi,resultapi,resultpostapi,resultdeleteapi}=require("../controllers/controller")
 
 // router.get("/questionapi",questionapi)
 // router.post("/questionapi",questionpostapi)
 
 router.route("/questionapi").get(questionapi).post(questionpostapi).delete(dropquestionapi)
+
+router.route("/resultapi").get(resultapi).post(resultpostapi).delete(resultdeleteapi)
 
 module.exports=router
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,50 @@
-const express=require("express")
-const dotenv = require("dotenv").config();
-const cors= require("cors")
-const route=require("./router/router.js")
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import { config } from 'dotenv';
+import router from './router/route.js';
 
-const app=express()
+
+/** import connection file */
+import connect from './database/conn.js';
+
+const app = express()
+
+
+/** app middlewares */
+app.use(morgan('tiny'));
 app.use(cors());
-app.use(express.json())
-app.use("/api",route)
+app.use(express.json());
+config();
 
-const port=process.env.PORT
 
-app.listen(port,()=>{
-    console.log(`server start successfuly on ${port}`)
+/** appliation port */
+const port = process.env.PORT || 8080;
+
+
+/** routes */
+app.use('/api', router) /** apis */
+
+
+app.get('/', (req, res) => {
+    try {
+        res.json("Get Request")
+    } catch (error) {
+        res.json(error)
+    }
 })
+
+
+/** start server only when we have valid connection */
+connect().then(() => {
+    try {
+        app.listen(port, () => {
+            console.log(`Server connected to http://localhost:${port}`)
+        })
+    } catch (error) {
+        console.log("Cannot connect to the server");
+    }
+}).catch(error => {
+    console.log("Invalid Database Connection");
+})
+
